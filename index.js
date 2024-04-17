71085e324ae70d4ae89139cdaef0a6bdee76d7d2
huntdavid175 <huntdavid175@gmail.com>
2024-04-17 13:56:39 -0400

    updated port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const app = express();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello EC2");
