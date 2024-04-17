0ba86ec1d3f81d74675c6517ccc949fbee5893ee
huntdavid175 <huntdavid175@gmail.com>
2024-04-17 13:59:05 -0400

    updated response

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello EC2");
+  return res.status(200).json({ message: "Hello EC2" });
 });
 
 app.listen(port, () => {
